f4849c44b68ac4494c627005d164438d1ebcaa60
jakewhite5 <jakewhite5@yahoo.com>
2021-06-07 11:33:22 -0700

    rough implementation

--- User.py
+++ User.py
@@ -1,1 +1,23 @@
-# your User class goes here
+import csv
+class User:
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.data_dict = {}
+        self.data_dict['username'] = self.username
+        self.data_dict['password'] = self.password
+        self.data_dict['email'] = self.email
+
+    
+    def save(self):
+        with open(f'{self.username}.csv', 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['username', 'password', 'email'])
+            writer.writeheader()
+            writer.writerow(self.data_dict)
+            
+
+jake = User('jakewhite5', 'xx', 'jakewhite5@yahoo.com')
+
+jake.save()
