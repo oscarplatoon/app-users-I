b0b51d3fb281ab45263855df7e1277cdb6f139f7
RenoAverill <reno_averill@yahoo.com>
2021-06-07 13:28:42 -0600

    Reno's app-users

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
-# your User class goes here
+class User:
+    def __init__(self,first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        
+    def create_new_user(self):
+        user_info = {'role': 'user'}
+
+        user_info['first_name'] = input('please enter your first name.')
+        user_info['last_name'] = input('please enter your last name.')
+        user_info['email'] = input('Please enter your email adress.')
+        user_info['password'] = input('Please create a secured password! your almost done!')
+
+        print(f"congradulations {user_info['first_name']} you have successfully created an account!")
