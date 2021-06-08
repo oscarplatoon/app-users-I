5fd1b0d1b9bce8193077577dcfb91bee9a6080af
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-07 22:04:48 -0400

    Andrew_app1_solution

--- User.py
+++ User.py
@@ -1,1 +1,15 @@
-# your User class goes here
+class User():
+
+    def __init__(self,name,email,drivers_license):
+        self.name = name
+        self.email = email
+        self.drivers_license = drivers_license
+
+
+
+a = User('Andrew','andy80@yahoo.mail','iDriveGut')
+b = User('Jen','jenjen@yahoo.mail','iDriveGut2')
+
+print(f'Name: {a.name}\tEmail: {a.email}\tLicense: {a.drivers_license}')
+
+print(f'Name: {b.name}\tEmail: {b.email}\tLicense: {b.drivers_license}')
