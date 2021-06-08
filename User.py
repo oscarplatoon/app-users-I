70fd35956ca1a72ca1710a40ab4bb307dc8d75a6
vzbrunstad <vzbrunstad@msn.com>
2021-06-07 22:10:56 -0500

    Vince B App User WIP

--- User.py
+++ User.py
@@ -1,1 +1,21 @@
-# your User class goes here
+import csv 
+import os.path
+
+class User():
+
+    def __init__ (self, name=None, email=None, licence=None):
+        self.users = []
+        self.name= name
+        self.email= email
+        self.licence= licence
+
+    def list_users (self):
+        for user in self.users:
+            print(user)
+    
+    def add_user (self, name, email, licence):
+        self.users.append(User(name, email, licence))
+        print (f"User {name} added")
+
+    def delete_user(self):
+        print("delete user")
